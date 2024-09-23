fb65d0f79f7c2e6e7a1ab44d88ef8b048216a863
havefunli <1641869338@qq.com>
2024-09-23 22:10:46 +0800

    写了作业第二题

---
+++ 搜索引擎实验二/Question_2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+const std::string DefaultPath = "./实验2_字符串查找/实验2.2_单文档查找用例.txt";
+using IndexTable = std::unordered_map<std::string, std::vector<int>>;
+
+// 读取文件
+std::ifstream ReadFile()
+{
+	std::ifstream in(DefaultPath);
+	if (!in.is_open())
+	{
+		std::cerr << "Open failed..." << std::endl;
+		exit(1);
+	}
+
+	return in;
+}
+
+// 建立词汇索引表
+void BuildTable(std::ifstream& file, IndexTable& table)
+{
+	int index = 1; // 计数
+	std::string line; // 读取每一行
+	std::string word; // 读取每一行的一个单词
+	while (getline(file, line))
+	{
+		std::stringstream ss(line);
+		while (ss >> word)
+		{
+			table[word].push_back(index);
+			index++;
+		}
+	}
+}
+
+bool SearchWord(IndexTable table)
+{
+	std::string word;
+	std::cout << "Please Enter# ";
+	std::cin >> word;
+
+	auto pos = table.find(word);
+	if (pos != table.end())
+	{
+		std::cout << pos->first << ": ";
+		std::for_each(pos->second.begin(), pos->second.end(), [](const int& val) {std::cout << val << " "; });
+		std::cout << std::endl;
+	}
+
+	return false;
+}
+
+int main()
+{
+	std::ifstream in = ReadFile();
+	
+	IndexTable table;
+	BuildTable(in, table);
+	
+	SearchWord(table);
+
+	return 0;
+}
